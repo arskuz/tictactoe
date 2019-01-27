0b9499d68509f02e7fffe00760afbea737242339
arskuz <hohlofish@yandex.ru>
2019-01-27 22:32:55 +0300

    Done with everything but winning conditions

--- tictactoe.js
+++ tictactoe.js
@@ -1,27 +1,20 @@
 const BUT_NUM = 9
 
-let x_num = []
-let o_num = []
-
 let symbol = [];
 
 function massive_reload(){
 for (let j = 0; j <= BUT_NUM; j++){
-  if(x_num.indexOf(j)!=-1)
-    {symbol[j] = "X"}
-  else if(o_num.indexOf(j)!=-1)
-      {symbol[j] = "0"}
-  else
-    {symbol[j] = "?"}
+   {symbol[j] = "?"}
    }
  }
 
 var div = document.createElement('div')
 document.body.appendChild(div)
 
+massive_reload()
+
 function reset_screen(){
   div.innerHTML = " ";
-  massive_reload()
   for (let x = 0; x < BUT_NUM; x++){
        div.innerHTML+= "<button id='"+x+"' onclick='butclick(this)'>"+symbol[x]+"</button>"
        if( (x+1)%3==0 )
@@ -32,18 +25,17 @@ function reset_screen(){
 reset_screen()
 
 function butclick(element){
-if (o_num.indexOf(element.id)!=-1 || x_num.indexOf(element.id)!=-1) {
+if (freecell(element.id) == false) {
   alert("This cell is already occupied!")
   return
 }
-x_num+=element.id
-alert(element.id)
+symbol[ element.id ] = "X"
 mastermind()
 reset_screen()
 }
 
 function freecell(cell_n){
-  if(o_num.indexOf(cell_n)==-1 && x_num.indexOf(4)==-1)
+  if(symbol[cell_n]=="?")
     {return true}
   else{
     return false
@@ -51,58 +43,91 @@ function freecell(cell_n){
 }
 
 function mastermind(){
-  if(o_num.indexOf(4)==-1 && x_num.indexOf(4)==-1 && o_num.indexOf(4)==-1){
-    o_num+=4
+
+  if(freecell(4)){
+    symbol[4]="0"
     return}
-  else if(scanline()>-1)
-    {o_num+=scanline()
+  else if(scanline("0")>-1)
+    {symbol[scanline("0")]="0"
     return}
-}
+  else if(scanline("X")>-1)
+    {symbol[scanline("X")]="0"
+    return}
+  else {
+    for (let k = 0; k <= BUT_NUM; k++){
+      if(symbol[k]=="?")
+        {symbol[k] = "0"
+        return}
+       }
+       return
+     }
+  }
 // | 036 147 258
 // - 012 345 678
-function scanline(){
+function scanline(toscan){
+
 //horisontal
-if(freecell(1)==true && x_num.indexOf(0)!=-1 && x_num.indexOf(2)!=-1)
-  {return 1}
-else if(freecell(0)==true && x_num.indexOf(1)!=-1 && x_num.indexOf(2)!=-1)
+if(freecell(0) && symbol[1]==toscan && symbol[2]==toscan)
   {return 0}
-else if(freecell(0)==true && x_num.indexOf(1)!=-1 && x_num.indexOf(0)!=-1)
+else if(freecell(1) && symbol[0]==toscan && symbol[2]==toscan)
+  {return 1}
+else if(freecell(2) && symbol[1]==toscan && symbol[0]==toscan)
   {return 2}
 
-else if(freecell(3)==true && x_num.indexOf(4)!=-1 && x_num.indexOf(5)!=-1)
+else if(freecell(3) && symbol[4]==toscan && symbol[5]==toscan)
   {return 3}
-else if(freecell(4)==true && x_num.indexOf(3)!=-1 && x_num.indexOf(5)!=-1)
+else if(freecell(4) && symbol[5]==toscan && symbol[3]==toscan)
   {return 4}
-else if(freecell(5)==true && x_num.indexOf(4)!=-1 && x_num.indexOf(3)!=-1)
+else if(freecell(5) && symbol[3]==toscan && symbol[4]==toscan)
   {return 5}
 
-else if(freecell(6)==true && x_num.indexOf(7)!=-1 && x_num.indexOf(8)!=-1)
+
+else if(freecell(6) && symbol[7]==toscan && symbol[8]==toscan)
   {return 6}
-else if(freecell(7)==true && x_num.indexOf(6)!=-1 && x_num.indexOf(8)!=-1)
+else if(freecell(7) && symbol[6]==toscan && symbol[8]==toscan)
   {return 7}
-else if(freecell(8)==true && x_num.indexOf(6)!=-1 && x_num.indexOf(7)!=-1)
+else if(freecell(8) && symbol[6]==toscan && symbol[7]==toscan)
   {return 8}
+
 //vertical
-else if(freecell(0)==true && x_num.indexOf(3)!=-3 && x_num.indexOf(6)!=-6)
+
+else if(freecell(0) && symbol[3]==toscan && symbol[6]==toscan)
   {return 0}
-else if(freecell(3)==true && x_num.indexOf(0)!=-1 && x_num.indexOf(6)!=-1)
+else if(freecell(3) && symbol[0]==toscan && symbol[6]==toscan)
   {return 3}
-else if(freecell(6)==true && x_num.indexOf(0)!=-1 && x_num.indexOf(3)!=-1)
+else if(freecell(6) && symbol[0]==toscan && symbol[3]==toscan)
   {return 6}
 
-  else if(freecell(1)==true && x_num.indexOf(4)!=-3 && x_num.indexOf(7)!=-3)
-    {return 1}
-  else if(freecell(4)==true && x_num.indexOf(1)!=-1 && x_num.indexOf(7)!=-1)
-    {return 4}
-  else if(freecell(7)==true && x_num.indexOf(1)!=-1 && x_num.indexOf(4)!=-1)
-    {return 7}
 
-  else if(freecell(2)==true && x_num.indexOf(5)!=-3 && x_num.indexOf(8)!=-3)
+else if(freecell(1) && symbol[4]==toscan && symbol[7]==toscan)
+  {return 1}
+else if(freecell(4) && symbol[1]==toscan && symbol[7]==toscan)
+  {return 4}
+else if(freecell(7) && symbol[1]==toscan && symbol[4]==toscan)
+  {return 7}
+
+else if(freecell(2) && symbol[5]==toscan && symbol[8]==toscan)
+  {return 2}
+else if(freecell(5) && symbol[2]==toscan && symbol[8]==toscan)
+  {return 5}
+else if(freecell(8) && symbol[2]==toscan && symbol[5]==toscan)
+  {return 8}
+
+//diagonal
+
+else if(freecell(0) && symbol[4]==toscan && symbol[8]==toscan)
+  {return 0}
+else if(freecell(4) && symbol[0]==toscan && symbol[8]==toscan)
+  {return 4}
+else if(freecell(8) && symbol[0]==toscan && symbol[4]==toscan)
+  {return 8}
+
+  else if(freecell(2) && symbol[4]==toscan && symbol[6]==toscan)
     {return 2}
-  else if(freecell(5)==true && x_num.indexOf(2)!=-1 && x_num.indexOf(8)!=-1)
-    {return 5}
-  else if(freecell(8)==true && x_num.indexOf(2)!=-1 && x_num.indexOf(5)!=-1)
-    {return 8}
+  else if(freecell(4) && symbol[2]==toscan && symbol[6]==toscan)
+    {return 4}
+  else if(freecell(6) && symbol[2]==toscan && symbol[4]==toscan)
+    {return 6}
 
 else {return -1}
 
